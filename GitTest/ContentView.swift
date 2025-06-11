4ddbeb049ebe8423c956017548dffe048e7b64b3
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2025-06-11 19:08:57 -0400

    My Second Commit

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Testing Git")
+            
+            Button("Click Me!") {
+                
+            }
         }
         .padding()
     }
